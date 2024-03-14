05fec1078f5f12af39fdee40d6c492fd35a38550
Dnyaneshwar Magar <89983890+dnyaneshwarmagar@users.noreply.github.com>
2024-03-15 02:05:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+for(let i=0; i<20; i++){
+	if(i%3 ===0){
+		console.log("Fizz")
+	}
+	else if(i%5 ===0){
+		console.log("Buzz")
+	}
+	else if(i%3 ===0 || i% 5 === 0){
+		console.log("FizzBuzz")
+	}
+}
